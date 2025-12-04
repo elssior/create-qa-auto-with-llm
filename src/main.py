42064a2ff378f61929bc50d58047e5209f0fcfe3
elssior <elssior@gmail.com>
2025-12-04 18:16:33 +0300

    Adding dependencies and correct main pipline

--- src/main.py
+++ src/main.py
@@ -3,16 +3,30 @@ from integrations.swagger_parser import parse_swagger
 from core.endpoint_processor import is_test_exists
 
 def main():
+    # Получаем список сервисов
     services = get_services()
+    if len(services) == 0:
+        raise "Сервисы не найдены."
 
+    # Перебираем сервисы
     for service in services:
+        # Получем эндпоинты сервиса
         endpoints = parse_swagger(service["swagger_path"])
-        
+        if len(endpoints) == 0:
+            raise f"Эндпоинтов в сервисе {service["name"]} не найдены или не существуют."
+
+        # Перебираем эндпоинты сервиса
         for endpoint in endpoints:
             print(f"{endpoint.method} {endpoint.path} - {endpoint.summary}")
             test_name = endpoint.path.strip('/').replace('/', '_')
+
+            # Проверяем существует ли тест
             test_exists = is_test_exists(service["name"], test_name)
-            print(f"  Test exists: {'Yes' if test_exists else 'No'}")
+            print(f"  Проверка: {'Тест уже существует' if test_exists else 'Тест не существует'}")
+            if test_exists:
+                continue
+
             
+
 if __name__ == "__main__":
     main()
